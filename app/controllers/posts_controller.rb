67d250d361daf834ec7f783c630a826a29141d66
Student <example@example.com>
2019-08-26 10:51:38 -0700

    setup posts, prompts, users and sesssions contollers

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,52 @@
+class PostsController < ApplicationController
+
+  def new
+    @prompt = Prompt.find(params[:prompt_id])
+    @post = prompt.posts.new
+    render :new
+  end
+
+  def create
+    @prompt = Prompt.find(params[:prompt_id])
+    @post = @prompt.posts.new(post_params)
+    if @prompt.save
+      flash[:notice] = "Post successfully added!"
+      redirect_to prompt_path(@prompt)
+    else
+      flash[:alert] = "Please fill out all fields"
+      render :new
+    end
+  end
+
+  def show
+    @prompt = Prompt.find(params[:prompt_id])
+    @post = Post.find(params[:id])
+    render :show
+  end
+
+  def edit
+    @prompt = Prompt.find(params[:prompt_id])
+    @post = Post.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to prompt_path(@post.prompt)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to prompt_path(@post.prompt)
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:body, :votes)
+  end
+end
