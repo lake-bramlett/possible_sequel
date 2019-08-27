9793505b8fb0320993378c15d1668b4b7305dbf4
Jacob Irwin <jakobinwins@gmail.com>
2019-08-27 10:05:48 -0700

    move content of manual controller to prompt model

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,26 +1,24 @@
-KEYS = Dotenv.load
 class MoviesController < ApplicationController
 
   include API
 
     def index
 
-
-
-      @prompt = Prompt.movie_prompt("the%20wolf%20of%20wall%20street")#change to Movies obj or move to prompt controller
-      movie_one = "titanic"
-      movie_two = "inception"
-      response_one = API::Interface.call_by_title(movie_one)
-      response_two = API::Interface.call_by_title(movie_two)
-      @response_one = JSON.parse(response_one)
-      @response_two = JSON.parse(response_two)
-      @title_one = JSON.parse(response_one)["Title"]
-      @title_two = JSON.parse(response_two)["Title"]
-      @actors_one = JSON.parse(response_one)["Actors"]
-      @actors_two = JSON.parse(response_two)["Actors"]
-      @plot_one = JSON.parse(response_one)["Plot"]
-      @plot_two = JSON.parse(response_two)["Plot"]
-
+      @prompt = Prompt.movie_prompt("the%20wolf%20of%20wall%20street")#send down to #random
     end
 
+    def manual
+      @prompt = Prompt.manual_prompt("titanic", "inception")
+      @title_one = @prompt[0]["Title"]
+      @title_two = @prompt[1]["Title"]
+      @actors_one = @prompt[0]["Actors"]
+      @actors_two = @prompt[1]["Actors"]
+      @plot_one = @prompt[0]["Plot"]
+      @plot_two = @prompt[1]["Plot"]
+      byebug
+      render :index
+    end
+
+    def random
+    end
 end
