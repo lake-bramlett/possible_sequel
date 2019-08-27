c0b7635a1a66fee912b01963603433e8a01fdcd5
Student <example@example.com>
2019-08-27 15:54:46 -0700

    remove unnecessary controller spec files and add passing spec for unauthorized access to prompts page.

---
+++ spec/features/posts_routes_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+# 
+# describe 'posts routes' do
+#
+#   before do
+#     FactoryBot.find_definitions
+#     user = FactoryBot.create(:user)
+#     visit ('/signin')
+#     fill_in(:email, :with => 'test@test.com')
+#     fill_in(:password, :with => 123456)
+#     click_button('Sign in')
+#   end
+#
+#   it 'creates a new post' do
+#     FactoryBot.find_definitions
+#     user = FactoryBot.create(:user)
+#     visit ("/users/#{user.id}/posts/new")
+#     fill_in(:body, :with => 'This movie is terrible and the other is awful, why am I here!?!?!')
+#     click_button('Create post')
+#     expect(page).to have_content('Post successfully added!')
+#   end
+#
+# end
