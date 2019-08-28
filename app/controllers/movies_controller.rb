d4127a2d1b1d393e3f31092e45cdc5c09c517176
Jacob Irwin <jakobinwins@gmail.com>
2019-08-27 19:34:52 -0700

    txt file made and random movie generator are working. next step is to see how I can catch and display the common actor because that isn't always apparant in the movie print out

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -18,7 +18,7 @@ class MoviesController < ApplicationController
     end
 
     def random
-      @prompt = Prompt.random_prompt("the%20wolf%20of%20wall%20street")#send down to #random
+      @prompt = Prompt.random_prompt#send down to #random
       @title_one = @prompt[0]["Title"]
       @title_two = @prompt[1]["Title"]
       @actors_one = @prompt[0]["Actors"]
--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -2,14 +2,18 @@ class Prompt < ApplicationRecord
   include API
   has_many :posts, dependent: :destroy
 
-  def self.random_prompt(title)
+  def self.random_prompt
+    movies_array = File.foreach('movies.txt').map { |line| line.split(", \n") }
+    random_movie = movies_array[rand(0..(movies_array.length-1))]
     @movies = []
-    response1 = API::Interface.call_by_title(title)
+
+    response1 = API::Interface.call_by_title(random_movie)
     movie1 = JSON.parse(response1)
     @movies.push(movie1)
     movie2 = find_similar_movie(movie1)
     length = movie2["results"][0]["known_for"].length
     movie2 = movie2["results"][0]["known_for"][rand(0..(length-1))]["title"]
+    byebug
     response2 = API::Interface.call_by_title(movie2)
     movie2 = JSON.parse(response2)
     @movies.push(movie2)
